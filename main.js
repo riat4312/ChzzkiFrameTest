10a524220fe46e100ca71626ff1caf76c5313a51
ICHINI <31132834+riat4312@users.noreply.github.com>
2023-10-03 00:34:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-let Money = 0; let Workers = 0; let Machine = 0; let iGrowth = 0;
+let Money = 0; let Workers = 0; let Machine = 0; let infGrowth = 0;
 let Money_display = document.getElementById("MoneyX");
 let Worker_display = document.getElementById("WorkerX");
 let Machine_display = document.getElementById("MachineX");
-let iGrowth_display = document.getElementById("iGrowthX");
+let infGrowth_display = document.getElementById("infGrowthX");
 
 function Display() {
 	Money_display.textContent = Money.toLocaleString('ko');
 	Worker_display.textContent = Workers.toLocaleString('ko');
 	Machine_display.textContent = Machine.toLocaleString('ko');
-	iGrowth_display.textContent = iGrowth.toLocaleString('ko');
+	infGrowth_display.textContent = iGrowth.toLocaleString('ko');
 }
 
 function BasicClick() { Money = Money + 100; }
 function WorkerClick() { Workers = Workers + 1; }
 function MachineClick() { Machine = Machine + 1; }
-function iGrowthClick() { iGrowth = iGrowth + 1; }
+function infGrowthClick() { infGrowth = iGrowth + 1; }
 
 function MoneyUpdate() {
 	Money = Money + Workers * 10;
 	/*if ( Money >= 1000000000000 ) { Money = 1000000000000; };*/
 }
 function EmployUpdate() { Workers = Workers + Machine; }
-function iGrowthUpdate() { Machine = Machine + iGrowth; }
+function infGrowthUpdate() { Machine = Machine + infGrowth; }
 
 setInterval(Display, 10);
 setInterval(MoneyUpdate, 100);
 setInterval(EmployUpdate, 100);
-setInterval(iGrowthUpdate, 100);
+setInterval(infGrowthUpdate, 100);
