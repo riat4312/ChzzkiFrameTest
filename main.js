4cdf650ae0ed3f731827d2e70d3a4c8fc4146c5e
ICHINI <31132834+riat4312@users.noreply.github.com>
2023-10-03 00:34:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let Money = 0; let Workers = 0; let Machine = 0; let infGrowth = 0;
+let Money = 0; let Workers = 0; let Machine = 0; let iGrowth = 0;
 let Money_display = document.getElementById("MoneyX");
 let Worker_display = document.getElementById("WorkerX");
 let Machine_display = document.getElementById("MachineX");
@@ -8,7 +8,7 @@ function Display() {
 	Money_display.textContent = Money.toLocaleString('ko');
 	Worker_display.textContent = Workers.toLocaleString('ko');
 	Machine_display.textContent = Machine.toLocaleString('ko');
-	infGrowth_display.textContent = iGrowth.toLocaleString('ko');
+	iGrowth_display.textContent = iGrowth.toLocaleString('ko');
 }
 
 function BasicClick() { Money = Money + 100; }
@@ -21,9 +21,9 @@ function MoneyUpdate() {
 	/*if ( Money >= 1000000000000 ) { Money = 1000000000000; };*/
 }
 function EmployUpdate() { Workers = Workers + Machine; }
-function infGrowthUpdate() { Machine = Machine + infGrowth; }
+function iGrowthUpdate() { Machine = Machine + infGrowth; }
 
 setInterval(Display, 10);
 setInterval(MoneyUpdate, 100);
 setInterval(EmployUpdate, 100);
-setInterval(infGrowthUpdate, 100);
+setInterval(iGrowthUpdate, 100);
